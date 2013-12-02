21820fa62de553df1ffa956678839c997804a200
Elliot Yap <yilliot@gmail.com>
2013-12-02 22:33:43 +0800

    offset function

--- SvgToCGContext.js
+++ SvgToCGContext.js
@@ -1,8 +1,9 @@
 
 function main() {
-    var bassClefPath = "M64.487,0.305c0.283-0.055,1.564-0.392,1.565-0.08c0,0.52,0,1.041,0,1.561c0,2.186,0,4.371,0,6.556c0,3.66,0,7.322,0,10.982c1.654-1.264,3.906-2.539,5.953-1.354c1.831,1.058,2.534,3.125,1.831,5.124c-0.686,1.945-2.305,3.503-3.844,4.783c-1.978,1.647-3.788,3.324-5.506,4.898c0-10.673,0-21.346,0-32.017C64.487,0.605,64.487,0.455,64.487,0.305 M66.053,29.667c1.738-1.915,4.28-4.351,4.491-7.274c0.092-1.27-0.503-2.761-1.883-2.968c-1.021-0.182-2.196,1.011-2.543,1.87c-0.14,0.349-0.065,0.816-0.065,1.184c0,0.934,0,1.867,0,2.801C66.053,26.741,66.053,28.203,66.053,29.667";
+    var flatPath = "M64.487,0.305c0.283-0.055,1.564-0.392,1.565-0.08c0,0.52,0,1.041,0,1.561c0,2.186,0,4.371,0,6.556c0,3.66,0,7.322,0,10.982c1.654-1.264,3.906-2.539,5.953-1.354c1.831,1.058,2.534,3.125,1.831,5.124c-0.686,1.945-2.305,3.503-3.844,4.783c-1.978,1.647-3.788,3.324-5.506,4.898c0-10.673,0-21.346,0-32.017C64.487,0.605,64.487,0.455,64.487,0.305 M66.053,29.667c1.738-1.915,4.28-4.351,4.491-7.274c0.092-1.27-0.503-2.761-1.883-2.968c-1.021-0.182-2.196,1.011-2.543,1.87c-0.14,0.349-0.065,0.816-0.065,1.184c0,0.934,0,1.867,0,2.801C66.053,26.741,66.053,28.203,66.053,29.667";
+    var flatPath = "M18.005,40.702c3.651-0.315,7.122,1.096,9.313,3.975c2.109,2.77,2.815,6.426,2.16,9.789c-0.678,3.474-2.952,6.374-5.963,8.28c-0.757,0.479-1.554,0.896-2.376,1.258c0.166,1.273,0.332,2.545,0.501,3.818c0.307,2.36,0.869,4.826,0.854,7.207c-0.019,3.235-1.106,6.481-4.04,8.283c-2.9,1.78-6.728,1.827-9.619,0.033c-2.957-1.832-5.047-5.547-3.602-8.917c1.442-3.358,5.894-4.426,8.24-1.287c1.32,1.765,1.468,4.46-0.122,6.12c-0.947,0.99-2.787,1.779-4.173,1.248c0.038,1.763,2.806,2.645,4.28,2.591c2.133-0.09,4.229-0.829,5.607-2.471c1.438-1.712,1.852-3.96,1.794-6.115c-0.043-1.644-0.377-3.283-0.616-4.909c-0.24-1.658-0.479-3.314-0.722-4.973c-7.669,1.806-14.747-2.696-17.805-9.461c-1.495-3.303-2.102-7.106-1.469-10.677c0.643-3.627,2.561-6.889,4.889-9.739c2.378-2.913,5.189-5.455,7.991-7.969c0.322-0.289,0.645-0.578,0.965-0.866c0.266-0.236,0.332-0.204,0.269-0.577c-0.049-0.29-0.097-0.58-0.141-0.869c-0.193-1.241-0.349-2.488-0.452-3.739c-0.179-2.194-0.233-4.43-0.015-6.623c0.293-2.926,1.128-5.879,2.384-8.551c0.627-1.333,1.391-2.642,2.413-3.729c0.72-0.768,1.887-1.78,3.026-1.832c0.725-0.033,1.272,0.942,1.565,1.45c0.666,1.151,1.124,2.42,1.521,3.679c0.926,2.944,1.588,6.116,1.514,9.208c-0.092,3.833-1.412,7.671-3.366,10.974c-1.028,1.735-2.254,3.357-3.637,4.839c-0.668,0.714-1.374,1.395-2.107,2.042c-0.356,0.313-0.047,1.199,0.014,1.653c0.096,0.697,0.188,1.392,0.281,2.087C17.577,37.522,17.793,39.111,18.005,40.702 M17.177,46.504c-3.562,0.835-6.433,4.767-5.283,8.322c0.252,0.78,0.702,1.485,1.271,2.083c0.291,0.304,0.608,0.581,0.944,0.833c0.384,0.285,0.894,0.486,1.239,0.803c0.279,0.255,0.172,0.673-0.166,0.817c-0.396,0.171-0.849-0.156-1.185-0.337c-0.833-0.448-1.59-1.023-2.245-1.694c-2.626-2.688-3.488-6.789-2.001-10.226c0.762-1.757,2.025-3.315,3.567-4.485c0.711-0.54,1.491-1.001,2.324-1.341c0.198-0.081,0.402-0.157,0.608-0.223c0.345-0.112,0.166-0.356,0.126-0.664c-0.093-0.7-0.185-1.4-0.276-2.101c-0.209-1.6-0.418-3.201-0.629-4.802C9.71,37.953,3.126,43.595,3.271,51.424c0.133,7.017,6.725,12.518,13.854,11.962c0.653-0.051,1.3-0.157,1.935-0.312c0.464-0.114,0.211-0.571,0.16-0.969c-0.053-0.399-0.104-0.798-0.159-1.198c-0.209-1.598-0.417-3.197-0.629-4.794C18.014,52.91,17.594,49.707,17.177,46.504 M24.401,10.783c0.065-1.904-1.371-4.025-3.523-3.233c-1.682,0.619-2.774,2.243-3.521,3.751c-2.039,4.133-2.262,8.636-1.479,13.101C20.174,21.168,24.23,16.3,24.401,10.783 M20.941,62.575c3.579-1.722,5.89-5.684,5.22-9.563c-0.628-3.616-3.596-6.402-7.401-6.605C19.485,51.797,20.211,57.187,20.941,62.575";
     var sgSvg = new SGSvg();
-    var lines = sgSvg.parser(bassClefPath);
+    var lines = sgSvg.parser(flatPath);
     var dots = sgSvg.linesToNormalizeDots(lines);
     var objectiveCLines = sgSvg.dotsToObjectiveCLines(dots);
     sgSvg.writeToObjectiveC(objectiveCLines);
@@ -11,8 +12,9 @@ function main() {
 var SGSvg = function(){
     this.lastX = 0;
     this.lastY = 0;
-    this.normalizeOffSetX = 0;
-    this.normalizeOffSetY = 0;
+    this.offsetX = 100;
+    this.offsetY = 100;
+    this.nPoint = 5;
 };
 
 SGSvg.prototype.parser = function(svgString) {
@@ -23,11 +25,6 @@ SGSvg.prototype.parser = function(svgString) {
 };
 
 SGSvg.prototype.linesToNormalizeDots = function(lines) {
-
-    var divider = lines[1].split("%");
-    var axises = divider[1].split(',');
-    this.normalizeOffSetX = 0 - (axises[0]*1);
-    this.normalizeOffSetY = 0 - (axises[1]*1);
 
     var dots = [];
     for(var i in lines) {
@@ -43,31 +40,31 @@ SGSvg.prototype.linesToNormalizeDots = function(lines) {
 
                 // extra attribute
                 if (dot.action === 'c') {
-                    dot.x1 = this.lastX + (axises[0]*1) + this.normalizeOffSetX;
-                    dot.y1 = this.lastY + (axises[1]*1) + this.normalizeOffSetY;
-                    dot.x2 = this.lastX + (axises[2]*1) + this.normalizeOffSetX;
-                    dot.y2 = this.lastY + (axises[3]*1) + this.normalizeOffSetY;
-                    dot.x0 = this.lastX + (axises[4]*1) + this.normalizeOffSetX;
-                    dot.y0 = this.lastY + (axises[5]*1) + this.normalizeOffSetY;
+                    dot.x1 = this.lastX + (axises[0]*1);
+                    dot.y1 = this.lastY + (axises[1]*1);
+                    dot.x2 = this.lastX + (axises[2]*1);
+                    dot.y2 = this.lastY + (axises[3]*1);
+                    dot.x0 = this.lastX + (axises[4]*1);
+                    dot.y0 = this.lastY + (axises[5]*1);
                     
                 } else {
-                    dot.x0 = this.lastX + (axises[0]*1) + this.normalizeOffSetX;
-                    dot.y0 = this.lastY + (axises[1]*1) + this.normalizeOffSetY;
+                    dot.x0 = this.lastX + (axises[0]*1);
+                    dot.y0 = this.lastY + (axises[1]*1);
                 }
 
 
             } else if(/[A-Y]/.test(dot.action)) {
 
                 if (dot.action === 'C') {
-                    dot.x1 = (axises[0]*1) + this.normalizeOffSetX;
-                    dot.y1 = (axises[1]*1) + this.normalizeOffSetY;
-                    dot.x2 = (axises[2]*1) + this.normalizeOffSetX;
-                    dot.y2 = (axises[3]*1) + this.normalizeOffSetY;
-                    dot.x0 = (axises[4]*1) + this.normalizeOffSetX;
-                    dot.y0 = (axises[5]*1) + this.normalizeOffSetY;
+                    dot.x1 = (axises[0]*1);
+                    dot.y1 = (axises[1]*1);
+                    dot.x2 = (axises[2]*1);
+                    dot.y2 = (axises[3]*1);
+                    dot.x0 = (axises[4]*1);
+                    dot.y0 = (axises[5]*1);
                 } else {
-                    dot.x0 = (axises[0]*1) + this.normalizeOffSetX;
-                    dot.y0 = (axises[1]*1) + this.normalizeOffSetY;
+                    dot.x0 = (axises[0]*1);
+                    dot.y0 = (axises[1]*1);
                 }
 
             } else {
@@ -76,6 +73,13 @@ SGSvg.prototype.linesToNormalizeDots = function(lines) {
             }
 
             dot.action = dot.action.toUpperCase();
+
+            if (this.offsetX > dot.x0) {
+                this.offsetX = dot.x0;
+            }
+            if (this.offsetY > dot.y0) {
+                this.offsetY = dot.y0;
+            }
 
             this.lastX = dot.x0;
             this.lastY = dot.y0;
@@ -86,16 +90,15 @@ SGSvg.prototype.linesToNormalizeDots = function(lines) {
 };
 
 SGSvg.prototype.dotsToObjectiveCLines = function(dots) {
-    var nPoint = 5;
     var lines = [];
     for(var i in dots) {
         var dot = dots[i];
         if (dot.action === 'M') {
-            lines.push('CGContextMoveToPoint(ctx, '+dot.x0.toFixed(nPoint)+'*scaleX, '+dot.y0.toFixed(nPoint)+'*scaleY);');
+            lines.push('CGContextMoveToPoint(ctx, [self offsetX:'+dot.x0.toFixed(this.nPoint)+'], [self offsetY:'+dot.y0.toFixed(this.nPoint)+']);');
         } else if (dot.action === 'C') {
-            lines.push('CGContextAddCurveToPoint (ctx, '+dot.x1.toFixed(nPoint)+'*scaleX, '+dot.y1.toFixed(nPoint)+'*scaleY, '+dot.x2.toFixed(nPoint)+'*scaleX, '+dot.y2.toFixed(nPoint)+'*scaleY, '+dot.x0.toFixed(nPoint)+'*scaleX, '+dot.y0.toFixed(nPoint)+'*scaleY);');
+            lines.push('CGContextAddCurveToPoint (ctx, [self offsetX:'+dot.x1.toFixed(this.nPoint)+'], [self offsetY:'+dot.y1.toFixed(this.nPoint)+'], [self offsetX:'+dot.x2.toFixed(this.nPoint)+'], [self offsetY:'+dot.y2.toFixed(this.nPoint)+'], [self offsetX:'+dot.x0.toFixed(this.nPoint)+'], [self offsetY:'+dot.y0.toFixed(this.nPoint)+']);');
         } else if (dot.action === 'L') {
-            lines.push('CGContextAddLineToPoint(ctx, '+dot.x0.toFixed(nPoint)+'*scaleX, '+dot.y0.toFixed(nPoint)+'*scaleY);');
+            lines.push('CGContextAddLineToPoint(ctx, [self offsetX:'+dot.x0.toFixed(this.nPoint)+'], [self offsetY:'+dot.y0.toFixed(this.nPoint)+']);');
         } else if (dot.action === 'Z') {
             lines.push(
                 "CGContextClosePath (ctx);\n"+
@@ -106,13 +109,18 @@ SGSvg.prototype.dotsToObjectiveCLines = function(dots) {
             );
         }
     }
+    lines.push(
+        "CGContextClosePath (ctx);\n"+
+        "    if (fillPath) {\n"+
+        "        CGContextFillPath (ctx);\n"+
+        "    }\n"+
+        "    CGContextStrokePath(ctx);\n"
+    );
     return lines;
 };
 
 SGSvg.prototype.writeToObjectiveC = function(lines) {
     var result = "-(void) drawRect:(CGRect)rect {\n"+
-        "    float scaleX = 0.5;\n"+
-        "    float scaleY = 0.5;\n"+
         "    BOOL fillPath = true;\n"+
         "    CGContextRef ctx = UIGraphicsGetCurrentContext();\n"+
         "    if (fillPath) {\n"+
@@ -124,7 +132,13 @@ SGSvg.prototype.writeToObjectiveC = function(lines) {
     for(var i in lines) {
         result += "    "+lines[i]+"\n";
     }
-    result += "}"
+    result += "}\n\n";
+    result += "-(float)offsetX:(float)x{\n";
+    result += "    return 2*(x - "+this.offsetX.toFixed(this.nPoint)+");\n";
+    result += "}\n\n";
+    result += "-(float)offsetY:(float)y{\n";
+    result += "    return 2*(y - "+this.offsetY.toFixed(this.nPoint)+");\n";
+    result += "}\n\n";
     var fs = require('fs');
     fs.writeFile("tmp.m", result , function(err) {
         if(err) {
